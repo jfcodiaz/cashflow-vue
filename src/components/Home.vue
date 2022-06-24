1bdc9c687c14b92372a9d22327a88b8e146fd4a9
JFcoDiaz <jfcodiaz@gmail.com>
2022-06-23 23:06:52 -0500

    03 setup a layout

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,16 @@
 <template>
-    <div>Hola Mundo</div>
+    <Layout>
+        <template #header>
+            <h1>Hola Mundo</h1>
+        </template>
+    </Layout>
 </template>
+
+<script>
+import Layout from "./Layout.vue"
+export default {
+    components: {
+        Layout
+    }
+}
+</script>
